ed60f531b34e8560a2b0bd519fbd0bd94ea542f7
saar120 <saar120@gmail.com>
2022-02-01 18:51:06 +0200

    filter

--- src/pages/AddWorkout/components/AddExerciseModal/SearchBar/SearchBar.jsx
+++ src/pages/AddWorkout/components/AddExerciseModal/SearchBar/SearchBar.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
-import { TextField } from "@mui/material";
+import { TextField, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import { ResultsHolder, Result } from "./SearchBar.styled";
 import data from "../../../../../mock-data/exercises.json";
 
 function SearchBar({ addExercise, closeModal }) {
+  const [category, setCategory] = useState("name");
   const [filteredData, setFilteredData] = useState([]);
+
+  const handleSelect = ({ target: { value } }) => {
+    setCategory(value);
+  };
 
   const handleFilter = ({ target: { value: searchTerm } }) => {
     const filtered = data.filter((ex) => {
-      return ex.name.toLowerCase().includes(searchTerm.toLowerCase());
+      if (category === "name") {
+        return ex[category].toLowerCase().includes(searchTerm.toLowerCase());
+      }
+      if (category === "primaryMuscles") {
+        return ex[category].includes(searchTerm.toLowerCase());
+      }
+      return ex;
     });
     setFilteredData(filtered);
   };
@@ -20,7 +31,14 @@ function SearchBar({ addExercise, closeModal }) {
 
   return (
     <>
-      <TextField name="exercise" variant="outlined" label="Exercise" onChange={handleFilter} />
+      <TextField name="exercise" variant="outlined" label="Search" onChange={handleFilter} />
+      <FormControl>
+        <InputLabel id="category">Search Category</InputLabel>
+        <Select labelId="category" value={category} onChange={handleSelect} defaultValue="name" label="Search Category">
+          <MenuItem value="name">Exercise</MenuItem>
+          <MenuItem value="primaryMuscles">Muscle</MenuItem>
+        </Select>
+      </FormControl>
       <ResultsHolder>
         {filteredData?.map((ex) => {
           return (
