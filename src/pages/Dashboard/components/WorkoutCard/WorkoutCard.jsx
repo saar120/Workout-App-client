7179577e2b2ef7683f376e4c953868b159f12e8a
Saar Amrani <saar120@gmail.com>
2022-02-08 11:01:08 +0200

    redo workout

--- src/pages/Dashboard/components/WorkoutCard/WorkoutCard.jsx
+++ src/pages/Dashboard/components/WorkoutCard/WorkoutCard.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { formatDistanceToNow } from "date-fns";
-
-import { WorkoutCardStyled, WorkoutTitle, SmallText, Group, ExercisesHolder } from "./WorkoutCard.styles";
+import { WorkoutCardStyled, WorkoutTitle, SmallText, Group, ExercisesHolder, Header } from "./WorkoutCard.styles";
 import Description from "../../../../components/Description/Description";
 import ExerciseSlider from "./ExerciseSlider/ExerciseSlider";
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function WorkoutCard({ workout, isLatest, redoHandler }) {
   return (
     <WorkoutCardStyled>
       <Group>
-        <WorkoutTitle>{workout?.title}</WorkoutTitle>
-        <Button onClick={redoHandler} variant="contained">
-          Redo Workout
-        </Button>
+        <Header>
+          <WorkoutTitle>{workout?.title}</WorkoutTitle>
+          <ButtonGroup size="small" variant="outlined" aria-label="small outlined primary button group">
+            <Button onClick={redoHandler}>Redo</Button>
+            <Button>
+              <MoreVertIcon />
+            </Button>
+          </ButtonGroup>
+        </Header>
         {isLatest && <SmallText>Latest workout</SmallText>}
         <SmallText>{formatDistanceToNow(new Date(workout.date)) + " ago"}</SmallText>
       </Group>
