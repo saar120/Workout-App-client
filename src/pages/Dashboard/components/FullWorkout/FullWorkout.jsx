f19a6eede3eb6677f363910827bf2cd72f96619f
Saar Amrani <saar120@gmail.com>
2022-02-10 13:29:12 +0200

    add table modal with full functionality

--- src/pages/Dashboard/components/FullWorkout/FullWorkout.jsx
+++ src/pages/Dashboard/components/FullWorkout/FullWorkout.jsx
@@ -1,28 +1,14 @@
 import React from "react";
-import { Modal, Paper, Table, TableBody, TableContainer, TableRow, TableCell, TableHead } from "@mui/material";
+import { Modal, Paper, TableContainer } from "@mui/material";
+import ExercisesTable from "./ExercisesTable";
 
 function FullWorkout({ open, closeModal, workout }) {
   return (
     <Modal open={open} onClose={closeModal} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <TableContainer component={Paper} sx={{ width: "clamp(350px,90vw,1000px)" }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center"># Set</TableCell>
-              <TableCell align="center">Reps</TableCell>
-              <TableCell align="center">Weight (Kg)</TableCell>
-              <TableCell align="center"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell># Set</TableCell>
-              <TableCell>Reps</TableCell>
-              <TableCell>Weight (Kg)</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+      <TableContainer component={Paper} sx={{ width: "clamp(350px,90vw,600px)", maxHeight: "600px" }}>
+        {workout.exercises.map((ex) => (
+          <ExercisesTable key={ex._id} exercise={ex} />
+        ))}
       </TableContainer>
     </Modal>
   );
